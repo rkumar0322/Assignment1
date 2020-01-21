39d696e0c68858a297cd29a1936e36131aa67803
Rahul Kumar <rk32298@gmail.com>
2020-01-21 15:18:34 -0500

    firstinput
    firstinput

---
+++ dataframeinput.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def parse_row(strparse):
+    row = strparse[1:-1].split("> <")
+    return row
+
+def parse_list(lostr):
+    lol = []
+    for i in lostr:
+        list1 = parse_row(i)
+        lol.append(list1)
+    return pd.DataFrame(lol)
+
+def parsefile():
+    f = open("data.txt","r")
+    list = []
+    line1 = f.readlines()
+    return [x.strip() for x in line1]
+
+print(parse_list(parsefile()))
