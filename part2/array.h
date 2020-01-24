843376ce79bac38f927cf93388ad2f9ad5428d07
Xinhan-Yan <yan.xi@husky.neu.edu>
2020-01-24 18:51:47 -0500

    Update array.h

--- part2/array.h
+++ part2/array.h
@@ -17,30 +17,43 @@ public:
     Object** array;
     size_t size;
 
+    //Constructor:
+    //initializing an empty array with input size
     Array(size_t limit) {
     }
     
 
+    //assign the element at index with input object
     void assign(Object element, size_t index) {
 
     }
     
+    //returns the element at index 
     Object get(size_t index) {
         
     }
     
+    //appends e to the end of the array 
     void push_back(Object o) {
         
     }
     
+    //inserts o at index
     void add(Object o, size_t index) {
         
     }
     
+    //removes all elements in the array 
     void clear() {
         
     }
+    
+    //compares o with this array for equality 
+    bool equals(Object o) {
+        
+    }
 
+    //returns the number of elements in the array
     size_t length() {
 
     }
