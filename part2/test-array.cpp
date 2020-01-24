e64f6cfba2fbc6f31ba631318465428f6e223496
Xinhan-Yan <yan.xi@husky.neu.edu>
2020-01-24 18:32:26 -0500

    Update test-array.cpp

--- part2/test-array.cpp
+++ part2/test-array.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include "object.h"
 #include "array.h"
+#include "string.h" 
+
 
 void test1() {
+    String s1 = new String("Hello");
+    String s2 = new String("World");
+    String s3 = new String("Hello");
+    t_true(s1->equals(s3));
+    t_false(s1->equals(s2));
+    OK("1");
+    
+}
+
+
+void test2() {
     
     String s1 = new String("Hello");
     String s2 = new String("World");
@@ -16,16 +29,15 @@ void test1() {
     a->assign(s3, 0);
     t_true(a->get(0)->equals(s3));
     a->clear();
-    t_true(
+    t_true(a->length() == 0);
+    OK("2");
     
 }
 
-
-
-
-
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    
+    test1();
+    test2();
     
     return 0;
 }
